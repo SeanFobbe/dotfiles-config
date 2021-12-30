5d8ae3c6c8b2fb75f2edbccdd5be080781babb37
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2021-12-30 01:38:36 +0100

    add dnf install to git clone script

--- 04_git_clone_all.sh
+++ 04_git_clone_all.sh
@@ -1,5 +1,7 @@
 # Based on this SO answer: https://stackoverflow.com/questions/19576742/how-to-clone-all-repos-at-once-from-github
 
+sudo dnf install curl jq git -y
+
 cd ~/github
 
 curl -sL "https://api.github.com/users/SeanFobbe/repos?per_page=1000" | jq -r '.[]|.clone_url' | xargs -L1 git clone
