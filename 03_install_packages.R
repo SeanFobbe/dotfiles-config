1fdab1f0366e0ba42322f2c5b04ba2f1c1dc5da0
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2021-12-30 03:04:16 +0100

    revise R install script

--- 03_install_packages.R
+++ 03_install_packages.R
@@ -1,11 +1,19 @@
-# Install R packages
-
+# Read full package list
 fullpackages <- readLines('packages-R-full.txt')
 
+# Print full package list
 print(fullpackages)
 
-dir.create("~/R/R-library-primary1")
+# Path to primary library
+library <- "~/R/R-library-primary"
 
+# Create folder if not already present
+dir.create(library)
+
+# Set primary library path
+.libPaths(library)
+
+# Install R packages to primary library path
 install.packages(fullpackages,
                  repos = 'https://cloud.r-project.org',
-                 lib = "~/R/R-library-primary")
+                 lib = library)
