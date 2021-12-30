5458ac8ac34a47766ecdb8445672fb0b928bc677
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2021-12-30 02:43:30 +0100

    revise R install script

--- 03_install_packages.R
+++ 03_install_packages.R
@@ -4,5 +4,8 @@ fullpackages <- readLines('packages-R-full.txt')
 
 print(fullpackages)
 
+dir.create("~/R/R-library-primary1")
+
 install.packages(fullpackages,
-                 repos = 'https://cloud.r-project.org')
+                 repos = 'https://cloud.r-project.org',
+                 lib = "~/R/R-library-primary")
