679a7e027315b3403e2753871e95ec0ae590d8c3
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-03-24 18:59:47 +0100

    add R installation

--- 02_install_packages_fedora.sh
+++ 02_install_packages_fedora.sh
@@ -10,6 +10,11 @@ sudo dnf update -y
 sudo dnf -y install $(cat packages-fedora-full.txt)
 
 
+# Install all R packages
+
+Rscript install_packages.R
+
+
 # Install all Python packages
 
 pip install $(cat packages-python-full.txt)
