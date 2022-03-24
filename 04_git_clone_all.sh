034abc547e701de12f56a42ef39ef73911f4f127
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-03-24 19:00:05 +0100

    rename

--- 04_git_clone_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Based on this SO answer: https://stackoverflow.com/questions/19576742/how-to-clone-all-repos-at-once-from-github
-
-sudo dnf install curl jq git -y
-
-cd ~/github || exit
-
-curl -sL "https://api.github.com/users/SeanFobbe/repos?per_page=1000" | jq -r '.[]|.clone_url' | xargs -L1 git clone
